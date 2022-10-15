e716bf79389f03caebb91d5f8f4e2d677bac1bbe
dariuskarpicius <dariusztmtyt@gmail.com>
2022-10-15 08:39:25 +0300

    Updated main class structure.

--- src/main/java/eu/hypezone/villagefever/VillageFever.java
+++ src/main/java/eu/hypezone/villagefever/VillageFever.java
@@ -1,14 +1,12 @@
 package eu.hypezone.villagefever;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -36,18 +34,6 @@ public class VillageFever {
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
-        //Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-    }
-
-    //You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-        //Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-
     }
 
     //You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
@@ -56,10 +42,6 @@ public class VillageFever {
 
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-
-            //Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
 
         }
     }
